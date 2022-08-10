18d4cafb204c643a4a662d109f27e5b0d8a7af34
Mario Yanez <marioy705@gmail.com>
2022-08-11 02:38:28 +0800

    Agregué concepto de block scope

---
+++ src/scope/block.js
@@ -0,0 +1,11 @@
+function fruits () {
+    if(true) {
+        var fruta1 = 'apple';
+        let fruta2 = 'pear';
+        const fruta3 = 'fresa';
+    }
+    console.log(fruta1)
+    console.log(fruta2)
+    console.log(fruta3)
+}
+fruits()
