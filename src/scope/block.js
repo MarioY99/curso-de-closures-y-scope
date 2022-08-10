ccf7cde653bd61df21d524c8926f84d428f7eebe
Mario Yanez <marioy705@gmail.com>
2022-08-11 02:39:15 +0800

    guardé bien mi block scope

--- src/scope/block.js
+++ src/scope/block.js
@@ -3,9 +3,10 @@ function fruits () {
         var fruta1 = 'apple';
         let fruta2 = 'pear';
         const fruta3 = 'fresa';
+        console.log(fruta2)
+        console.log(fruta3)
     }
     console.log(fruta1)
-    console.log(fruta2)
-    console.log(fruta3)
 }
 fruits()
+//Cuando hablamos de block scope nos quiere decir que una variable solo podrá ser accedida a ella dentro del bloque de código en el que se esté trabajando.
