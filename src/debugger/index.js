e84317839bdf4f1d4fba4953ec03470255a41737
Mario Yanez <marioy705@gmail.com>
2022-08-12 04:04:39 +0800

    debugger

---
+++ src/debugger/index.js
@@ -0,0 +1,9 @@
+var a = 'hello';
+
+function hello() {
+    let b = 'Hello World'
+    const c = 'Hello World!'
+    debugger
+}
+hello()
+//De esta forma entramos a la herramienta de chrome para debuggear
